172bbd53e1a714a5ab8bc3ec61d89b4f7b28ea54
NichSonv <nichsonsilva@gmail.com>
2023-01-20 15:20:05 -0300

    update
    - leadForm v1.0b

--- leadForm/lead.go
+++ leadForm/lead.go
@@ -8,12 +8,13 @@ import (
 var (
 	p = fmt.Print
 	pl = fmt.Println
-	pf = fmt.Printf
+	// pf = fmt.Printf
 	sc = fmt.Scan
-	scl = fmt.Scanln
+	// scl = fmt.Scanln
 	scf = fmt.Scanf
 	consign string
 	pers Person
+	err = "==Error! Invalid answer. Try again==\n"
 )
 type Person struct {
 	name string
@@ -27,42 +28,51 @@ func printPerson(pers Person) {
 	pl("Email:", pers.email)
 }
 
-func main() {
-	pl("Hello! Do you wish to fill this form? Y/N")
-	sc(&consign); p("\n")
-	if !(consign == "Y" || consign == "y" || consign == "N" || consign == "n") {
-		pl("Error! Invalid answer. Try again")
-		main()
-	}
-	switch consign {
-	case "Y","y":
-		pl("Alright! What's your name?")
-		scf("%s", &pers.name); p("\n")
-		// if pers.name != {
-			
-		pl("Your age?")
-		scf("%d", &pers.age); p("\n")
-		pl("And your email?")
-		scf("%s", &pers.email); p("\n")
-	case "N","n":
-		pl("Alright, have a nice day!")
-		os.Exit(0)
-	}
+func dataGet() {
+	pl("Alright! What's your name?")
+	scf("%s", &pers.name); p("\n")
+	// if pers.name != {
+		
+	pl("Your age?")
+	scf("%d", &pers.age); p("\n")
 
+	pl("And your email?")
+	scf("%s", &pers.email); p("\n")
+
+	pl("Ok, all set! Please review your info:")
 	confirm()
 }
 
 func confirm() {
-	pl("Ok, all set! Please review your info:")
 	printPerson(pers); p("\n")
-
 	pl("Confirm? Y/N")
+	sc(&consign, "\n")
+	if !(consign == "Y" || consign == "y" || consign == "N" || consign == "n") {
+		pl(err)
+		confirm()
+	}
+	switch consign {
+	case "Y", "y":
+		pl("Alright! Thank you very much!")
+		os.Exit(0)
+	case "N", "n":
+		pl("Ok, let's restart...")
+		dataGet()
+	}
+}
+
+func main() {
+	pl("Hello! Do you wish to fill this form? Y/N")
 	sc(&consign); p("\n")
 	if !(consign == "Y" || consign == "y" || consign == "N" || consign == "n") {
-		pl("Error! Invalid answer. Try again")
-		confirm()
-	} else {
-		pl("Alright! Thank you very much!")
+		pl(err)
+		main()
+	}
+	switch consign {
+	case "Y","y":
+		dataGet()
+	case "N","n":
+		pl("Alright, have a nice day!")
 		os.Exit(0)
 	}
 }
